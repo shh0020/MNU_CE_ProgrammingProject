ddccf2b486ee67417ad720a8335d14fb72753025
shh0020 <shh0020@naver.com>
2021-09-16 18:40:22 +0900

    준형아 이거 됬나 봐바

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
-//이건 메인임
+//이게 수정이 되는건지 모르겠어요!!!!! 준형아 무슨 연동
 public class main {
 
-	public static void ) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new MainFrame();
 	}
